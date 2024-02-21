aab4041d91022c4ded28e5c3829f4fff8245185d
vibiks <39383389+vibiks@users.noreply.github.com>
2024-02-21 09:11:07 +0530

    Add files via upload

--- dag_gcs.py
+++ dag_gcs.py
@@ -17,46 +17,9 @@ gcs_file = "{{ dag_run.conf['name'] }}"
 
 run_this = "echo " + gcs_file
 
-def getfilesize(fname):
-    client = storage.Client()
-    bucket = client.bucket('inceptezcustomer1')
-    desired_file = fname
-    for blob in bucket.list_blobs():
-        if desired_file== blob.name and blob.size > 0:
-            print("Name: "+ blob.name +" Size blob obj: "+str(blob.size) + "bytes")
-          
-def read_gcs_file(fname):
-    hook = GCSHook()
-    # perform gcs hook download
-    resp_byte = hook.download_as_byte_array(
-        bucket_name="inceptezcustomer1",
-        object_name=fname,
-    )
-
-    resp_string = resp_byte.decode("utf-8")
-    print(resp_string)
-
-
-
-task1 = PythonOperator(
-    task_id="get_file_size",
-    python_callable=getfilesize,
-    op_kwargs={"fname": gcs_file},
-    dag=dag,
-)
-
-task2 = PythonOperator(
-    task_id="read_file_print",
-    python_callable=read_gcs_file,
-    op_kwargs={"fname": gcs_file},
-    dag=dag,
-)
-
-
-
-task3 = GCSToBigQueryOperator(
+task1 = GCSToBigQueryOperator(
         task_id="load_trans_data",
-        bucket="inceptezcustomer1",
+        bucket="<update-bucketname>",
         source_objects=[gcs_file],
         destination_project_dataset_table="retaildb.tbltrans_stg",
         schema_fields=[
@@ -73,15 +36,14 @@ task3 = GCSToBigQueryOperator(
         write_disposition="WRITE_TRUNCATE",
 )
 
-task4 = BigQueryInsertJobOperator(
+task2 = BigQueryInsertJobOperator(
           task_id='call_sp_to_processdata',
           configuration={
           "query": {
              "query": "CALL `retaildb.sp_processdata`();",
-            #"query": "{% include 'Scripts/Script.sql' %}",
             "useLegacySql": False,
         }
     }
 )
 
-task1 >> task2 >> task3 >> task4
+task1 >> task2
